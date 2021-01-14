def5ee28c720b74ab5aed115d6c343013c1fc882
Jaff <jaffreyrodriguez42@gmail.com>
2021-01-14 23:41:42 +0800

    Level 6 - OAuth 2.0 and how to implement sing in with Google

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const mongoose = require("mongoose");
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require('mongoose-findorcreate'); // dont forget to require after installing this package
+
 
 const app = express();
 
@@ -27,17 +30,42 @@ mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true, use
 
 const userSchema = new mongoose.Schema({
 	email: String,
-	password: String
+	password: String,
+	googleId: String  // add googleId field to add a googleId field on the mongoDB database
 });
 
 userSchema.plugin(passportLocalMongoose); // this code should be placed after creating the schema and before the model
+userSchema.plugin(findOrCreate);  // add this package as a plugin of your schema
 
 const User = mongoose.model("User", userSchema);
 
-passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+
+passport.use(User.createStrategy());  // still include this one
+
+passport.serializeUser(function(user, done) {  // update the serialize and deserialize
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",  // make sure that it has the port 3000 after localhost
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"  // google plus is sunsetting
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
 
 app.get("/", function (req, res) {
     res.render("home");
@@ -58,6 +86,16 @@ app.get("/secrets", function(req, res){
 		res.redirect("/register");
 	}
 });
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['profile'] }));
+
+app.get('/auth/google/secrets', 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect to "/secrets".
+    res.redirect('/secrets');
+  });
 
 app.post("/register", function(req, res){
 	User.register({username: req.body.username}, req.body.password, function(err, user){
