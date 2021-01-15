aa4135ea143ead2e367e8d2d50e229fcdd5ee743
Jaff <jaffreyrodriguez42@gmail.com>
2021-01-15 22:59:52 +0800

    Finishing up the app

--- app.js
+++ app.js
@@ -33,7 +33,8 @@ const userSchema = new mongoose.Schema({
 	email: String,
 	password: String,
 	googleId: String,  // add googleId field to add a googleId field on the mongoDB database
-	facebookId: String
+	facebookId: String,
+  secret: String
 });
 
 userSchema.plugin(passportLocalMongoose); // this code should be placed after creating the schema and before the model
@@ -93,12 +94,37 @@ app.get("/register", function (req, res) {
     res.render("register");
 });
 
+app.get("/submit", function(req, res){
+  if(req.isAuthenticated()){
+    res.render("submit");
+  }else{
+    res.redirect("/login");
+  }
+});
+
+app.post("/submit", function(req, res){
+  const submittedSecret = req.body.secret;
+
+  User.findById(req.user.id, function(err, foundUser){
+    if(err){
+      console.log(err);
+    }else{
+      if(foundUser){
+        foundUser.secret = submittedSecret;
+        foundUser.save(function(){
+          res.redirect("/secrets");
+        });
+      }
+    }
+  });
+});
+
+
+
 app.get("/secrets", function(req, res){
-	if(req.isAuthenticated()){
-		res.render("secrets");
-	}else{
-		res.redirect("/register");
-	}
+  User.find({secret: {$ne: null}}, function(err, foundUsers){
+    res.render("secrets", {usersWithSecret: foundUsers});
+  });	
 });
 
 app.get('/auth/google',
